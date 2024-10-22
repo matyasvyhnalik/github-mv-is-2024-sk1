ca6669644b503e9690567190c4ebef5348ff25ca
Matyáš Vyhnalík <matyas.vyhnaik@tul.cz>
2024-10-22 21:54:38 +0200

    soucet_cifer_ukol #2

--- IS-Projekty/program002b-soucet-cifer/Program.cs
+++ IS-Projekty/program002b-soucet-cifer/Program.cs
@@ -1,46 +1,41 @@
 ﻿using System;
-
-class Program {
-    static void Main() {
-        
+class Program
+{
+    static void Main()
+    {
         string again = "a";
-        
-        while(again == "a") {
+        while (again == "a")
+        {
             Console.Clear();
-            Console.WriteLine("***********************************");
-            Console.WriteLine("***** Ciferný součet a součin *****");
-            Console.WriteLine("***********************************");
-            Console.WriteLine("******* Matyáš Vyhnalík ***********");
-            Console.WriteLine("***********************************");
+            Console.WriteLine("*******************************");
+            Console.WriteLine("**** Součet cifer *************");
+            Console.WriteLine("******** Matyáš Vyhnalík ******");
+            Console.WriteLine("*******************************");
             Console.WriteLine();
-
-
+            Console.WriteLine();
             Console.Write("Zadejte celé číslo: ");
+            string input = Console.ReadLine();
             int number;
-            while(!int.TryParse(Console.ReadLine(), out number)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+            while (!int.TryParse(input, out number)){
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+                input = Console.ReadLine();
             }
-
+            Console.WriteLine();
             Console.WriteLine("Zadané hodnoty: ");
-            Console.WriteLine("Cislo: {0}", number);
-
+            Console.WriteLine("číslo: {0}", number);
             string numberString = Math.Abs(number).ToString();
-            int sum = 0;
-
-            foreach (char digitChar in numberString) {
+            int sum = 0; 
+            foreach (char digitChar in numberString){
+                
                 int digitValue = digitChar - '0';
-                sum += difitValue;
-                Console.WriteLine("Momentální zbytek = {0}", digitChar);
-            
+                sum += digitValue;
+                Console.WriteLine("Aktuální zbytek = {0}", digitChar);
             }
-
-            Console.WriteLine("");
-
+            Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", number, sum);
+            Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Program můžete opakovat stiskem klávesy 'A'");
             again = Console.ReadLine();
-
         }
-
     }
-}    
+}
