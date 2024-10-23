735afd7b6aeeca90deaeea01971bcf9e77cffce1
Matyáš Vyhnalík <matyas.vyhnaik@tul.cz>
2024-10-23 10:15:02 +0200

    #4 prototyp_generator

--- IS-Projekty/program004-generator/Program.cs
+++ IS-Projekty/program004-generator/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program {
     static void Main() {
@@ -45,11 +46,46 @@ class Program {
 
             Random randomNumber = new Random();
 
+            int down = 0;
+            int nula = 0;
+            int up = 0;
+            int suda = 0;
+            int licha = 0;
+
             Console.WriteLine("\n\nNáhodná čísla: ");
             for(int i=0; i<n; i++) {
                 myArray[i] = randomNumber.Next(dn,hn);
                 Console.Write("{0}; ", myArray[i]);
+
+                //if(myArray[i]>0)
+                  //  up++;
+                //if(myArray[i]<0)
+                  //  down++;
+                //if(myArray[i]==0)
+                //nula++;
+
+                if(myArray[i]>0)
+                    up++;
+                else if(myArray[i]<0)
+                    down++;
+                else
+                    nuly++;
+
+                if(myArray[i] % 2 == 0)
+                    suda++;
+                else
+                    licha++;
+
+
             }
+
+            Console.WriteLine();
+            Console.WriteLine("\n\nPočet kladných čísel: {0}", up);
+            
+            Console.WriteLine("Počet záporných čísel: {0} ", down);
+            Console.WriteLine("Počet nul: {0}", nula);          
+            Console.WriteLine("Počet sudých čísel: {0}", suda);
+            Console.WriteLine("Počet lichých čísel: {0}", licha);  
 
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu A");
